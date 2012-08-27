b8d8cb4d53c0e8884278ea5b0db5c3ea4212aab8
Jeffrey Mealo <jeffreymealo@gmail.com>
2012-08-27 10:39:59 -0400

    added unit tests

--- phoney.js
+++ phoney.js
@@ -1,1 +1,124 @@
-TODO
+function decodeVanity(phone, format) {
+    if (typeof phone !== 'string' || phone === undefined || phone.length === 0) {
+        return new Error("decodeVanity expects argument 'phone' as string!");
+    }
+
+    phone = phone
+        .replace(/[abc]/gi, '2')
+        .replace(/[def]/gi, '3')
+        .replace(/[ghi]/gi, '4')
+        .replace(/[jkl]/gi, '5')
+        .replace(/[mno]/gi, '6')
+        .replace(/[pqrs]/gi, '7')
+        .replace(/[tuv]/gi, '8')
+        .replace(/[wxyz]/gi, '9');
+
+    return formatPhone(phone, format);
+}
+
+function extractPhoneNumbers(haystack, format, options) {
+
+    options = options || {removeDupes:true};
+
+    if (options && typeof options !== 'object') {
+        return new Error("extractPhoneNumbers expects argument 'options' as object or undefined.");
+    }
+
+    if (format && typeof format !== 'string') {
+        return new Error("extractPhoneNumbers expects argument 'format' as string or undefined.");
+    }
+
+    if (typeof haystack !== 'string' || haystack === undefined) {
+        return new Error("extractPhoneNumbers expects argument 'phone' as string!");
+    }
+
+    var phone_number = null;
+
+    var phone_regex1 = /((?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?)/gi;
+    var phone_regex2 = /(\d{1}\s\(\d{3}\)\s\d{3}-\d{4})/gi;
+    //JS doesn't let us put a regexp on multiple lines without compiling it each time, this is ugly but performant
+    //hack: facebook sometimes formats phone numbers as 1 (555) 555-5555 which is not matched by the first regex
+    //TODO: add to first regexp
+
+    phone_number = haystack.match(phone_regex1);
+    if (phone_number == null) {
+        phone_number = haystack.match(phone_regex2);
+    }
+
+    if (phone_number == null) {
+        return false;
+    } else {
+        var len = phone_number.length;
+        var return_val = [], phone = '';
+        for (var x = 0; x < len; x++) {
+            phone = formatPhone(phone_number[x], format, options);
+            if (options.removeDupes === false || return_val.indexOf(phone) === -1) {
+                return_val.push(phone);
+            }
+        }
+        return return_val;
+    }
+}
+
+function formatPhone(phone, format, options) {
+    format = format || 'aaa-bbb-cccc';
+    //handle null values, assign defaults
+
+    options = options || {decodeVanity:false};
+
+    if (typeof phone !== 'string' || phone.length === 0) {
+        return new Error("formatPhone expects argument 'phone' as string!");
+    }
+
+    if (typeof format !== 'string') {
+        return new Error("formatPhone expects argument 'format' as string or undefined!");
+    } else if (!format.match(/[+i1]*?.*[a]{0,3}.*[b]{3}.*[c]{4}/i)) {
+        return new Error("formatPhone was passed an invalid 'format' argument; see documentation.");
+    }
+
+    var a, b, c;
+
+    if (options.decodeVanity) {
+        phone = decodeVanity(phone);
+    }
+
+    phone = phone.replace(/[^a-z\d]/gi, '');
+    //remove non-alphanumeric characters and underscores
+
+    if (phone.length == 11) {
+        //remove country code to separate digit groups
+        phone = phone.slice(1);
+    }
+
+    if (phone.length == 10) {
+        a = phone.slice(0, 3);
+        phone = phone.slice(3);
+    }
+
+    if (phone.length == 7) {
+        b = phone.slice(0, 3);
+        c = phone.slice(3, 7);
+    }
+
+    return format
+        .replace(/i/i, '1')
+        .replace(/a{3}/i, a)
+        .replace(/b{3}/i, b)
+        .replace(/c{4}/i, c);
+}
+
+function stripNonNumeric(str) {
+    //strip non-numeric characters from a string
+
+    if (typeof str === 'string') {
+        return str.replace(/[^0-9]+/g, '');
+    } else {
+        return new Error("stripNonNumeric expects string as argument!");
+    }
+}
+
+module.exports = {
+    formatPhone:formatPhone,
+    decodeVanity:decodeVanity,
+    extractPhoneNumbers:extractPhoneNumbers
+}
--- test/test.js
+++ test/test.js
@@ -1,1 +1,68 @@
-//TODO
+var assert = require('assert');
+var phoney = require('../phoney.js');
+
+assert.throws(phoney.decodeVanity(''));
+assert.throws(phoney.decodeVanity(null));
+assert.throws(phoney.decodeVanity());
+assert.throws(phoney.decodeVanity(17322919635));
+assert.throws(phoney.decodeVanity({}));
+assert.throws(phoney.decodeVanity([]));
+
+assert.throws(phoney.formatPhone(''));
+assert.throws(phoney.formatPhone(null));
+assert.throws(phoney.formatPhone());
+assert.throws(phoney.formatPhone(17322919635));
+assert.throws(phoney.formatPhone([]));
+assert.throws(phoney.formatPhone({}));
+
+assert.throws(phoney.formatPhone('(732) 291-9635', ''));
+assert.throws(phoney.formatPhone('(732) 291-9635', null));
+assert.throws(phoney.formatPhone('(732) 291-9635'));
+assert.throws(phoney.formatPhone('(732) 291-9635', 17322919635));
+assert.throws(phoney.formatPhone('(732) 291-9635', []));
+assert.throws(phoney.formatPhone('(732) 291-9635', {}));
+assert.throws(phoney.formatPhone('(732) 291-9635', 'QQQ-ZZZ-WWWW'));
+
+assert.equal(phoney.formatPhone('1-800-JEFF-NOW', 'I-AAA-BBB-CCCC'), '1-800-JEF-FNOW');
+assert.equal(phoney.formatPhone('1-800-JEFF-NOW', 'I-AAA-BBB-CCCC', {decodeVanity: true}), '1-800-533-3669');
+assert.equal(phoney.formatPhone('1-800-533-3669', 'I-AAA-BBB-CCCC', {decodeVanity: false}), '1-800-533-3669');
+
+assert.equal(phoney.formatPhone('(732) 291-9635', 'AAA-BBB-CCCC'), '732-291-9635');
+assert.equal(phoney.formatPhone('(732) 291.9635', '(AAA) BBB-CCCC'), '(732) 291-9635');
+
+assert.equal(phoney.formatPhone('(732) 291-9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('(732) 291.9635', '(aaa) bbb-cccc'), '(732) 291-9635');
+assert.equal(phoney.formatPhone('(732) 291 9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('732 291 9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('732.291.9635', 'i-aaa-bbb-cccc'), '1-732-291-9635');
+assert.equal(phoney.formatPhone('1.732.291.9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('+1.732.291.9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('1-732-291-9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('732-291-9635', '+1.aaa.bbb.cccc'), '+1.732.291.9635');
+assert.equal(phoney.formatPhone('1-732-291-9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('+1-732-291-9635', 'aaa-bbb-cccc'), '732-291-9635');
+assert.equal(phoney.formatPhone('1 732 291 9635', 'aaa-bbb-cccc'), '732-291-9635');
+
+assert.equal(phoney.decodeVanity('800-JEFF-NOW'), '800-533-3669');
+assert.equal(phoney.decodeVanity('800-jeff-now'), '800-533-3669');
+assert.equal(phoney.decodeVanity('1-800-JEFF-NOW', 'i-aaa-bbb-cccc'), '1-800-533-3669');
+assert.equal(phoney.decodeVanity('1-800-jeff-now', 'i-aaa-bbb-cccc'), '1-800-533-3669');
+assert.equal(phoney.decodeVanity('1-800-JEFF-NOW', '1-aaa-bbb-cccc'), '1-800-533-3669');
+assert.equal(phoney.decodeVanity('1-800-jeff-now', '1-aaa-bbb-cccc'), '1-800-533-3669');
+
+var test_text = 'Lorem ipsum +1-732-291-9635 dolor sit amet, consectetur adipiscing elit. (732) 291 9635 Suspendisse'
++ 'nec lectus ante, ut viverra odio. 1-732-290-9630 1-702-290-0635 +1-732-201-9635 Duis bibendum quam tellus. Donec ut '
++ ' rutrum tellus. Sed imperdiet convallis ullamcorper. Praesent consectetur quam 732-391-963 et felis tincidunt vitae '
++ ' interdum nisl fermentum. Sed ut elit lectus, 1712312(702)-242-2313414092 in dictum turpis. Ut purus urna, '
++ ' pellentesque et iaculis nec, eleifend at velit.';
+
+
+assert.deepEqual(phoney.extractPhoneNumbers(test_text),
+    [ '732-291-9635', '732-290-9630', '702-290-0635', '732-201-9635' ]);
+
+assert.deepEqual(phoney.extractPhoneNumbers(test_text, null, {removeDupes: false}),
+    ['732-291-9635','732-291-9635','732-290-9630','702-290-0635','732-201-9635']);
+
+assert.deepEqual(phoney.extractPhoneNumbers(test_text, 'I-AAA-BBB-CCCC', {removeDupes: false}),
+    ['1-732-291-9635','1-732-291-9635','1-732-290-9630','1-702-290-0635','1-732-201-9635']);
+
